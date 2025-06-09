316711a4d5b8a87786b57314a931529eaa0b84d0
rfsyhmmm <rafsyahfachri10@gmail.com>
2025-06-10 06:11:51 +0700

    menambahkan dan mengubah file GameMain, Cell, Board,, Seed, State menjadi versi OOP Graphical

--- Seed.java
+++ Seed.java
@@ -1,14 +1,11 @@
+/**
+ * This enum is used by:
+ * 1. Player: takes value of CROSS or NOUGHT
+ * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
+ *
+ * Ideally, we should define two enums with inheritance, which is,
+ *  however, not supported.
+ */
 public enum Seed {   // to save as "Seed.java"
-    CROSS("X"), NOUGHT("O"), NO_SEED(" ");
-
-    // Private variable
-    private String icon;
-    // Constructor (must be private)
-    private Seed(String icon) {
-        this.icon = icon;
-    }
-    // Public Getter
-    public String getIcon() {
-        return icon;
-    }
+    CROSS, NOUGHT, NO_SEED
 }
--- State.java
+++ State.java
@@ -1,3 +1,6 @@
-public enum State {
+/**
+ *  The enum State contains the various game states of the TTT game
+ */
+public enum State {  // to save as "State.java"
     PLAYING, DRAW, CROSS_WON, NOUGHT_WON
 }
